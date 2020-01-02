a94b867984fb2775a9438119073be41c3dac6389
BigbrotherShin <shinjhhp5@gmail.com>
2020-01-02 16:44:03 +0900

    redux-thunk Promise Refactoring 1

--- src/modules/posts.js
+++ src/modules/posts.js
@@ -9,6 +9,7 @@
 */
 
 import * as postsAPI from '../api/posts'; // api/posts 안의 함수 모두 불러오기
+import { createPromiseThunk, reducerUtils } from '../lib/asyncUtils';
 
 /* action type */
 
@@ -21,6 +22,53 @@ const GET_POSTS_ERROR = 'GET_POSTS_ERROR'; // 요청 실패
 const GET_POST = 'GET_POST';
 const GET_POST_SUCCESS = 'GET_POST_SUCCESS';
 const GET_POST_ERROR = 'GET_POST_ERROR';
+
+// 아주 쉽게 thunk 함수를 만들 수 있게 되었습니다.
+export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
+export const getPost = createPromiseThunk(GET_POST, postsAPI.getPostById);
+
+// initialState 쪽도 반복되는 코드를 initial() 함수를 사용해서 리팩토링 했습니다.
+const initialState = {
+  posts: reducerUtils.initial(),
+  post: reducerUtils.initial(),
+};
+
+export default function posts(state = initialState, action) {
+  switch (action.type) {
+    case GET_POSTS:
+      return {
+        ...state,
+        posts: reducerUtils.loading(),
+      };
+    case GET_POSTS_SUCCESS:
+      return {
+        ...state,
+        posts: reducerUtils.success(action.payload), // action.posts -> action.payload
+      };
+    case GET_POSTS_ERROR:
+      return {
+        ...state,
+        posts: reducerUtils.error(action.error)
+      };
+    case GET_POST:
+      return {
+        ...state,
+        post: reducerUtils.loading()
+      };
+    case GET_POST_SUCCESS:
+      return {
+        ...state,
+        post: reducerUtils.success(action.payload), // action.post -> action.payload 로 변경됐습니다.
+      };
+    case GET_POST_ERROR:
+      return {
+        ...state,
+        post: reducerUtils.error(action.error)
+      };
+    default:
+      return state;
+  }
+}
 
 // thunk 를 사용 할 때, 꼭 모든 액션들에 대하여 액션 생성함수를 만들 필요는 없습니다.
 // 그냥 thunk 함수에서 바로 액션 객체를 만들어주어도 괜찮습니다.
@@ -108,3 +156,5 @@ export default function posts(state = initialState, action) {
       return state;
   }
 }
+
+// 이런 반복되는 코드는 따로 함수화하여 코드를 리팩토링하는 것이 좋은 자세
